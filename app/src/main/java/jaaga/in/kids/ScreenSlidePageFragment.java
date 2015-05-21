5f240f3dc127d07a1aac924281344eaeeb17067a
sneha06 <snehaacharya06@gmail.com>
2015-05-21 14:35:01 +0530

    image sliding

--- app/src/main/java/jaaga/in/kids/ScreenSlidePageFragment.java
+++ app/src/main/java/jaaga/in/kids/ScreenSlidePageFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -23,7 +24,11 @@ public class ScreenSlidePageFragment extends Fragment {
     //String[] aToz = getResources().getStringArray(R.array.alphabet);
     String[] aToz = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
             "U","V","W","X","Y","Z"};
+    int[] images = {R.mipmap.a,R.mipmap.b,R.mipmap.c,R.mipmap.d,R.mipmap.e,R.mipmap.f,R.mipmap.g,R.mipmap.h,R.mipmap.i,
+            R.mipmap.j,R.mipmap.k,R.mipmap.l,R.mipmap.m,R.mipmap.n,R.mipmap.o,R.mipmap.p,R.mipmap.q,R.mipmap.r,
+            R.mipmap.s,R.mipmap.t,R.mipmap.u,R.mipmap.v,R.mipmap.w,R.mipmap.x,R.mipmap.y,R.mipmap.z};
     public static final String ARG_PAGE = "page";
+
 
     /**
      * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
@@ -67,7 +72,9 @@ public class ScreenSlidePageFragment extends Fragment {
         TextView textUpper = (TextView) rootView.findViewById(R.id.uppercase);
         TextView textlower = (TextView) rootView.findViewById(R.id.lowercase);
         ImageButton play = (ImageButton) rootView.findViewById(R.id.play);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.images);
 
+        imageView.setImageResource(images[mPageNumber]);
         textUpper.setText(aToz[mPageNumber]);
         textlower.setText(aToz[mPageNumber].toLowerCase());
 
